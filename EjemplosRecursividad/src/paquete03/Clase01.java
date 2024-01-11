4e911ca9c23f122eee340c25224bba0e30b4bda4
Luis Cordova <lcordovacarrion@gmail.com>
2024-01-11 15:52:37 -0500

    Clase05

---
+++ EjemplosRecursividad/src/paquete03/Clase01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author lcord
+ */
+public class Clase01 {
+    public static void main(String[] args){
+        
+    }
+    public static String[][] obtenerCadena(){
+        String[][] cadena = {{"LOJA","CUENCA","QUITO"},
+            {"GUAYAQUIL","ZAMORA","MACHALA"}};
+        return cadena;
+    }
+}
